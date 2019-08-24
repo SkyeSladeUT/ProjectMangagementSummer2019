7509c682a8e7e754fd74fdf1c64ae6ec0ad57b99
Unknown <skyesladeut@outlook.com>
2019-08-24 15:07:35 -0600

    Making Trees Transparent

---
+++ Unity Project/SummerGameScripts/Assets/Scripts/Swap_Geo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swap_Geo : MonoBehaviour
+{
+    public List<GameObject> objs;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger");
+        if (other.CompareTag("Material_Change"))
+        {
+            Debug.Log("Change");
+            objs[0].SetActive(false);
+            objs[1].SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Material_Change"))
+        {
+            objs[0].SetActive(true);
+            objs[1].SetActive(false);
+        }
+    }
+}
